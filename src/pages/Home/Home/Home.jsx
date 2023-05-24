f4c303a7759adc9f09d3bfa087bc862d39dd1a0c
keyamoni05866 <keya05866@gmail.com>
2023-05-24 23:53:07 +0600

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+       <div >
+       <Banner></Banner>
+     
+       </div>
+        </div>
+    );
+};
+
+export default Home;
